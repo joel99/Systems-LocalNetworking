bd84d942d8176ef0ba898f82e52b8cc52360b5f2
Joel Ye <jye5@stuy.edu>
2016-12-16 15:35:41 -0500

    yo wth man

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,18 +2,35 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+//see 12-14 notes
+int client_handshake( int * address ){
+  
+  int fd = open("waluigi", O_WRONLY);
+
+  mkfifo("test", 0644);
+  
+  int fromClient = open("test", O_RDONLY);
+
+  write(fd, "test", 4);
+  
+  
+  return 0;
+}
+
+
 //gets a client pipe - server connects as reader, client connects
 int server_handshake( int * address ){
   mkfifo("waluigi", 0644);
 
-  int fd = open("waluigi", O_WRONLY);
+  int fd = open("waluigi", O_RDONLY);
 
-  write(fd, "Welcome!\n", 9);
+  //write(fd, "Welcome!\n", 9);
+  char buf[64];
+  read("waluigi", buf, sizeof(buf));
+
+  int toClient = open(buf, O_WRONLY);
+  write( toClient, "SCP\n", 4);
+  remove("waluigi");
   
   return 0;
 }
-//see 12-14 notes
-int client_handshake( int * address ){
-  return 0;
-}
-
